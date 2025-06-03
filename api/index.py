cee7f0cf47c43b9b809901766cbef3efa1e3c420
R1sco <riscorisky@gmail.com>
2025-06-03 21:40:12 +0700

    Tambahkan User-Agent untuk scraping & tentukan versi Werkzeug

--- api/index.py
+++ api/index.py
@@ -156,7 +156,10 @@ def get_lyrics():
 
 def scrape_lyrics(url):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         
         soup = BeautifulSoup(response.text, 'html.parser')
